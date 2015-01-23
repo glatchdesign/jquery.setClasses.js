ea81c037c5bfc0703fbdcfa77f23511396b92bac
osgworks <sgtnyk0531@gmail.com>
2015-01-23 17:06:53 +0900

    Changes in 1.0.0

--- jquery.setClasses.js
+++ jquery.setClasses.js
@@ -1,35 +1,44 @@
+/*!
+*
+* Copyright (c) 2014 Yuki Sugitani
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+* @author: Yuki Sugitani (https://github.com/osgworks/jquery.setClasses.js)
+* @version: 1.0.0
+*/
 ;(function($) {
 	var pluginName = 'setClasses',
 		defaults = {
-			target : 'setTarget',
+			target : '.setTarget',
 			firstClass : 'first',
 			lastClass : 'last'
 		};
 
-	function SetClasses(element, options) {
+	function setClasses(element, options) {
 		this.element = element;
 		this.options = $.extend({}, defaults, options);
 		this.init();
 	}
 
-	SetClasses.prototype.init = function() {
-		var self = this,
-			$target = $(self.element).find(self.options.target);
-
+	setClasses.prototype.init = function() {
+		var that = this,
+			$target = $(that.element).find(that.options.target);
+			
 		$target.each(function(index) {
 			var item = $(this);
-			
+
 			if(index === 0) {
-				item.addClass(self.options.firstClass);
+				item.addClass(that.options.firstClass);
 			}　else if(index === $target.length-1) {
-				item.addClass(self.options.lastClass);
+				item.addClass(that.options.lastClass);
 			}
 		});
 	};
 
 	$.fn[pluginName] = function (options) {
 		return this.each(function() {
-			new SetClasses(this, options);
+			new setClasses(this, options);
 		});
 	};
 })(jQuery);
